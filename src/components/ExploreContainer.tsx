1682fa33356e950d0efe9281b347bb6d5766f5b4
Nathan Walker <walkerrunpdx@gmail.com>
2021-02-28 21:39:44 -0800

    chore: wire up the openNativeModalView example

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,22 @@
 import './ExploreContainer.css';
+import { native } from '@nativescript/capacitor';
 
 interface ContainerProps {
   name: string;
 }
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  function handleClick(e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    e.preventDefault();
+    native.openNativeModalView();
+  }
   return (
     <div className="container">
       <strong>{name}</strong>
       <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p className="ion-padding-top">
+        <a onClick={handleClick}>Open Native Modal</a>
+      </p>
     </div>
   );
 };
